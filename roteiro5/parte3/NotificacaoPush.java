ada07e82790b2f10dd26192ddd7b5277eda2a5df
felipedsanta <felipe.carro3@outlook.com>
2025-05-05 08:37:45 -0300

    Roteiro 5 completo

---
+++ roteiro5/parte3/NotificacaoPush.java
@@ -0,0 +1,9 @@
+package roteiro5.parte3;
+
+public class NotificacaoPush implements Notificacao {
+    @Override
+    public void enviar(String mensagem) {
+        System.out.println("Enviando notificação push: " + mensagem);
+    }
+
+}
